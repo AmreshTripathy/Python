059c3a0e9a60ca1e64a30454c02664d54f5b0a54
AmreshTripathy <65341864+AmreshTripathy@users.noreply.github.com>
2021-04-12 22:24:26 +0530

    Add files via upload

---
+++ StringModification.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+for i in range(0, n):
+    string = input()
+    for j in range(0, len(string)):
+        if (j % 2 == 0):
+            print (string[j],end='')
+    print(" ",end='')
+    for j in range(0, len(string)):
+        if(j % 2 != 0):
+            print (string[j],end='')
+    print("")
