fb2d841b47f5eb389f5990efaf520565501fa83d
Amresh Tripathy <amreshtripathy184@gmail.com>
2021-06-14 22:59:02 +0530

    Add files via upload

---
+++ 2ndLarge.py
@@ -0,0 +1,9 @@
+a=int(input())
+b=int(input())
+c=int(input())
+if (a<b  and a > c)or (a>b and a<c):
+    print (a)
+if (b<a  and b > c)or (b>a and b<c):
+    print (b)
+if (c<b  and c > a)or (c>b and c<a):
+    print (c)
