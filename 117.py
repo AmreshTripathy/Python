860470fb6f5c1a648e253e32f5c5194ee4dd917f
Amresh Tripathy <amreshtripathy184@gmail.com>
2021-06-23 21:40:22 +0530

    Add files via upload

---
+++ 117.py
@@ -0,0 +1,19 @@
+l=[10,20,30,40,50]
+print("the values of list are :")
+for i in l:
+    print(i)
+
+t=(10,20,30,40,50)
+print("the values of tuple are :")
+for j in t:
+    print(j)
+
+s={10,20,30,40,50}
+print("the values of set are : ")
+for x in s:
+    print(x)
+
+d={'a':10,'b':20,'c':30,'d':40,'e':50}
+print("the values of dictionary are : ")
+for key,val in d.items():
+    print(key," = ",val)
