e86420fef7f52da393be5b50ac2f13bddfeb3306
Amresh Tripathy <amreshtripathy184@gmail.com>
2021-06-24 21:58:49 +0530

    Add files via upload

---
+++ Untitled11.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print ('amresh')
+
+
+# In[3]:
+
+
+n = int(input('Enter a number'))
+for i in range(1,11):
+    print (f'{n} X {i} = {i*n}')
+
+
+# In[ ]:
+
+
+
+
